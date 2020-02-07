4eef0084c3b4dd868feccc4c7ed84baac2c563d2
Alexander Pietsch <apietsch@wald-wird-mobil.de>
2020-02-07 17:01:20 +0100

    use constructor based injection

--- src/main/java/be/pengo/demoDependencyInjection/MyMessageApplication.java
+++ src/main/java/be/pengo/demoDependencyInjection/MyMessageApplication.java
@@ -14,18 +14,11 @@ public class MyMessageApplication {
 
 	private MessageService service;
 	
-	@Autowired
 	public MyMessageApplication(MessageService svc){
 		logger.info("Message Service gets wired through constructor!");
 		this.service=svc;
 	}
-	
-	@Autowired
-	public void setService(MessageService svc){
-		logger.info("Message Service gets wired through setter!");
-		this.service=svc;
-	}
-	
+
 	public boolean processMessage(String msg, String rec){
 		logger.info("will sending message.. " + msg);
 		return this.service.sendMessage(msg, rec);
